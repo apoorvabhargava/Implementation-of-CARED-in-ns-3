d37a3b7762487cacf2298c1ea14d3214eb00899f
mansi-bhatt <mansibhatt94@gmail.com>
2017-11-03 10:14:43 +0530

    Remove Unwanted files

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/subdir/ns3-dev-subdir-debug', 'build/scratch/ns3-dev-scratch-simulator-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
